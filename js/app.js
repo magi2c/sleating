0a7651666de1610a21fe5639e87f0b877cfe376d
magi.carbonell <magi.carbonell@yaencontre.com>
2013-11-06 14:50:31 +0100

    show/hide group

--- js/app.js
+++ js/app.js
@@ -17,93 +17,23 @@ app.factory('menu', function() {
 app.controller('AppCtrl', ['$scope', '$http', 'menu', function ($scope, $http, menu) {
 
     $scope.getFoods = function() {
-        var self = this;
         $http.get("doc/foods.json").success(function(jsonF) {
-            self.foods = jsonF;
-        });
+            $scope.foods = jsonF;
+        
+        $scope.groupShow = {};
+        for (groupKey in $scope.foods) {
+            $scope.groupShow[groupKey] = false;
+        }
 
-        return self.foods;
+        });
     };
 
-    $scope.foods = $scope.getFoods();
+    $scope.groupShowToggle = function(groupKey) {
+        $scope.groupShow[groupKey] = ! $scope.groupShow[groupKey]
+    }
+
+
+    $scope.getFoods();
     $scope.menu = menu;
-
-
-
-
-/*    $scope.resetTypeDisabled = function() {
-        $scope.typeDisabled = {hydrate: "",neutral: "",fat: "",fruit: ""};
-    };
-    $scope.resetTypeDisabled();
-
-
-    $scope.isFoodsCheck = function(food) {
-        var index = $scope.foodsCheck.indexOf(food)
-        return (index >= 0);
-    };
-
-
-    $scope.inFoodsCheck = function(food) {
-        var index = $scope.foodsCheck.indexOf(food)
-        if (index >= 0){
-            $scope.foodsCheck.splice(index, 1);
-        } else {
-            $scope.foodsCheck.push(food);
-        }
-    };
-
-    $scope.activeTypeDisabled = function() {
-        angular.forEach($scope.foodsCheck, function(food) {
-
-            switch (food.type) {
-                case 'fruit':
-                    $scope.typeDisabled['hydrate'] = 'food-disabled';
-                    $scope.typeDisabled['fat'] = 'food-disabled';
-                    $scope.typeDisabled['neutral'] = 'food-disabled';
-                    break;
-                case 'hydrate':
-                    $scope.typeDisabled['fruit'] = 'food-disabled';
-                    $scope.typeDisabled['fat'] = 'food-disabled';
-                    break;
-                case 'neutral':
-                    $scope.typeDisabled['fruit'] = 'food-disabled';
-                    break;
-                case 'fat':
-                    $scope.typeDisabled['fruit'] = 'food-disabled';
-                    $scope.typeDisabled['hydrate'] = 'food-disabled';
-                    break;
-            }
-        });
-    };
-
-
-    $scope.isFoodDisabled = function(food) {
-        if ($scope.addFoodClass(food) == 'food-disabled') {
-            return true;
-        }
-
-        return false;
-    };
-
-    $scope.addFoodClass = function(food) {
-        if ($scope.isFoodsCheck(food)) {
-            return 'food-check';
-        }
-
-        if (food.phase == 'Stay' && $scope.thin == true) {
-            return 'food-disabled';
-        }
-
-        return $scope.typeDisabled[food.type];
-    };
-
-
-    $scope.change = function(food) {
-        $scope.inFoodsCheck(food);
-        $scope.resetTypeDisabled();
-        $scope.activeTypeDisabled();
-    };*/
-
-
 
 }]);
